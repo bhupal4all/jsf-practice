e3ae2426bc6bb4fc1272ad00a134b94166bc65b6
Ranga Bhupal Reddy Gangavarapu <bhupal4all@gmail.com>
2015-10-20 12:33:44 +0530

    JSF Custom Tag - Extending TagHandler and providing behavior

---
+++ JsfCustomTagHandler/src/main/java/com/ranga/jsf/tag/MessageTagHanlder.java
@@ -0,0 +1,76 @@
+package com.ranga.jsf.tag;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIOutput;
+import javax.faces.view.facelets.ComponentHandler;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagAttribute;
+import javax.faces.view.facelets.TagConfig;
+import javax.faces.view.facelets.TagHandler;
+
+public class MessageTagHanlder extends TagHandler {
+
+	public MessageTagHanlder(TagConfig config) {
+		super(config);
+		System.out.println("MessageTagHanlder ctor");
+	}
+
+	@Override
+	public void apply(FaceletContext ctx, UIComponent parent)
+			throws IOException {
+		System.out.println("MessageTagHanlder tag impl");
+
+		TagAttribute bold = getAttribute("bold");
+		TagAttribute italic = getAttribute("italic");
+		TagAttribute underline = getAttribute("underline");
+
+		Boolean isBold = false, isItalic = false, isUnderline = false;
+
+		if (bold != null)
+			isBold = new Boolean(bold.getValue());
+		if (italic != null)
+			isItalic = new Boolean(italic.getValue());
+		if (underline != null)
+			isUnderline = new Boolean(underline.getValue());
+
+		TagAttribute value = getAttribute("value");
+
+		StringBuilder outputBuilder = new StringBuilder();
+
+		if (ComponentHandler.isNew(parent)) {
+			if (isBold) {
+				outputBuilder.append("<b>");
+			}
+			if (isItalic) {
+				outputBuilder.append("<i>");
+			}
+			if (isUnderline) {
+				outputBuilder.append("<u>");
+			}
+
+			if (value != null)
+				outputBuilder.append(value.getValue());
+			else
+				outputBuilder.append("default message: Hello World");
+
+			if (isBold) {
+				outputBuilder.append("</b>");
+			}
+			if (isItalic) {
+				outputBuilder.append("</i>");
+			}
+			if (isUnderline) {
+				outputBuilder.append("</u>");
+			}
+
+			UIOutput output = new UIOutput();
+			output.setValue(outputBuilder.toString());
+			parent.getChildren().add(output);
+		}
+
+		nextHandler.apply(ctx, parent);
+	}
+
+}
