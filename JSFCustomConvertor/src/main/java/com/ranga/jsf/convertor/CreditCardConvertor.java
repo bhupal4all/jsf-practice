24fa477b42e8f3656ac33eea8cbc2d6904ffdae8
Ranga Bhupal Reddy Gangavarapu <bhupal4all@gmail.com>
2015-10-25 20:50:57 +0530

    JSF Convertor passing seperator and error message through f:attribute tag

--- JSFCustomConvertor/src/main/java/com/ranga/jsf/convertor/CreditCardConvertor.java
+++ JSFCustomConvertor/src/main/java/com/ranga/jsf/convertor/CreditCardConvertor.java
@@ -14,15 +14,25 @@ public class CreditCardConvertor implements javax.faces.convert.Converter {
 			String value) {
 		System.out.println("getAsObject: " + value);
 
+		String seperator = (String) component.getAttributes().get("seperator");
+		System.out.println("seperator passed is '" + seperator + "'");
+
 		String creditCardNumber = (String) value;
 		creditCardNumber = creditCardNumber.trim();
-		creditCardNumber = creditCardNumber.replaceAll(" ", "");
+		creditCardNumber = creditCardNumber.replaceAll(seperator, "");
+
+		String errorMessage = (String) component.getAttributes().get("errorMessage");
+		System.out.println("errorMessage passed is '" + errorMessage + "'");
+
+		if (errorMessage == null || errorMessage.isEmpty()) {
+			errorMessage = "Credit Card Length is NOT matched for '"
+					+ creditCardNumber + "'";
+		}
 
 		if (creditCardNumber.length() == 0 || creditCardNumber.length() > 16) {
 			FacesMessage message = new FacesMessage(
 					FacesMessage.SEVERITY_ERROR, "Custom Validation Failed.",
-					"Credit Card Length is NOT matched for '"
-							+ creditCardNumber + "'");
+					errorMessage);
 
 			throw new ConverterException(message);
 		}
